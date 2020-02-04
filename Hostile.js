ec7bfd56f7cc10ccdb1368658c1e959794f1309b
Nicolas <nicolas.baudvin@gmail.com>
2020-02-04 16:59:30 +0100

    position de départ fixes

--- Hostile.js
+++ Hostile.js
@@ -14,27 +14,12 @@ class Hostile {
     }
 
     draw = () => {
-        const playerPosX = app.currentPlayer.getPosX();
-        const playerPosY = app.currentPlayer.getPosY();
-        console.log(playerPosX, playerPosY);
-        if (this.x !== playerPosX || this.y !== playerPosY) {
-            // if (this.x + 60 > playerPosX + 30 && this.x - 60 < playerPosX - 30){
-            //     return this.draw();
-            // }
-            // if (this.y + 60 > playerPosY + 30 && this.y - 60 < playerPosY - 30) {
-            //     return this.draw();
-            // }
             console.log("X position", this.x)
             console.log("Y position", this.y)
             this.img.classList.add('hostile');
             this.ctx.drawImage(this.img, this.x, this.y);
             this.ctx.stroke();
             console.log(this.img.width, this.img.height)
-        } else {
-            console.log("X position", this.x, playerPosX)
-            console.log("Y position", this.y, playerPosY)
-            return this.draw();
-        }
     }
 
     move = () => {
