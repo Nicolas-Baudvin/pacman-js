4cb9dfa055c430916343cbf2cad745ddd809633f
Nicolas <nicolas.baudvin@gmail.com>
2020-02-08 15:06:24 +0100

    rework du fonctionnement des déplacements des fantômes. (déplacements aléatoires pour le moment)

--- Hostile.js
+++ Hostile.js
@@ -10,8 +10,7 @@ class Hostile {
         this.speed = 4;
         this.height = "";
         this.width = "";
-        this.done = false;
-        this.moveDirection = this.name === "blinky" || this.name === "clyde" ? "left" : "right";
+        this.moveDirection = this.name === "blinky" || this.name === "clyde" ? "right" : "left";
 
         this.img = new Image();
         this.img.addEventListener('load', this.draw);
@@ -23,44 +22,97 @@ class Hostile {
         this.ctx.stroke();
         this.height = this.img.height;
         this.width = this.img.width;
-        this.interval = setInterval(() => {
-            if (this.done) {
-                clearInterval(this.interval);
+        if (this.name === "blinky") {
+            this.interval = setInterval(() => {
+                this.move();
+            }, 10);
+        }
+        if (this.name === "inky") {
+            setTimeout(() => {
                 this.interval = setInterval(() => {
-                    this.startMoving();
+                    this.move();
                 }, 10);
-            }
-            this.leaveSpawn();
-        }, 10);
+            }, 500);
+        }
+        if (this.name === "clyde") {
+            setTimeout(() => {
+                this.interval = setInterval(() => {
+                    this.move();
+                }, 10);
+            }, 1000);
+        }
+        if (this.name === "pinky") {
+            setTimeout(() => {
+                this.interval = setInterval(() => {
+                    this.move();
+                }, 10);
+            }, 1500);
+        }
+
     }
 
-    checkCollision = () => {
+    /**
+    * Logique de vérification de collision avec un mur à droite
+    * 
+    * @returns Boolean - true => collision / false => pas de collision
+    */
+    checkRightCollision = () => {
 
-        let pix;
-        /**
-         * Logique de vérification de collision avec un mur
-         */
-        if (this.moveDirection === "right") {
-
-            pix = this.ctx.getImageData(this.x + 67, this.y, 1, 60);
-        }
-        else if (this.moveDirection === "left") {
-            pix = this.ctx.getImageData(this.x - 7, this.y, 1, 60);
-        }
-        else if (this.moveDirection === "up") {
-            pix = this.ctx.getImageData(this.x, this.y - 7, 60, 1);
-        }
-        else if (this.moveDirection === "down") {
-            pix = this.ctx.getImageData(this.x, this.y + 67, 60, 1);
-        }
+        const pixRight = this.ctx.getImageData(this.x + 61, this.y, 1, 60);
 
         let rgba = [];
 
-        for (let i = 0; i < pix.data.length; i += 4) {
-            const red = pix.data[i];
-            const green = pix.data[i + 1];
-            const blue = pix.data[i + 2];
-            const alpha = pix.data[i + 3];
+        for (let i = 0; i < pixRight.data.length; i += 4) {
+            const red = pixRight.data[i];
+            const green = pixRight.data[i + 1];
+            const blue = pixRight.data[i + 2];
+            const alpha = pixRight.data[i + 3];
+            const array = [red, green, blue, alpha];
+            rgba.push(array);
+        }
+        if (rgba[0][0] === 0 && rgba[0][1] === 0 && rgba[0][2] === 0 && rgba[0][3] === 255) {
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][3] === 255) {
+            return true
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][3] === 255) {
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][3] === 255) {
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - 1)][0] === 0 && rgba[Math.floor(rgba.length - 1)][1] === 0 && rgba[Math.floor(rgba.length - 1)][2] === 0 && rgba[Math.floor(rgba.length - 1)][3] === 255) {
+            return true;
+        }
+        else if (rgba[0][0] !== 255 && rgba[0][1] !== 255 && rgba[0][2] !== 255 && rgba[0][3] !== 255) {
+            if (rgba[0][0] !== 0 && rgba[0][1] !== 0 && rgba[0][2] !== 0 && rgba[0][3] !== 0) {
+                return true;
+            }
+            else {
+                return false
+            }
+        }
+        else {
+            return false;
+        }
+
+    }
+    /**
+    * Logique de vérification de collision avec un mur à gauche
+    * 
+    * @returns Boolean - true => collision / false => pas de collision
+    */
+    checkLeftCollision = () => {
+        const pixLeft = this.ctx.getImageData(this.x - 1, this.y, 1, 60);
+
+        let rgba = [];
+
+        for (let i = 0; i < pixLeft.data.length; i += 4) {
+            const red = pixLeft.data[i];
+            const green = pixLeft.data[i + 1];
+            const blue = pixLeft.data[i + 2];
+            const alpha = pixLeft.data[i + 3];
             const array = [red, green, blue, alpha];
             rgba.push(array);
         }
@@ -93,11 +145,264 @@ class Hostile {
         else {
             return false;
         }
+    }
 
+    /**
+    * Logique de vérification de collision avec un mur en haut
+    * 
+    * @returns Boolean - true => collision / false => pas de collision
+    */
+    checkUpCollision = () => {
+        const pixUp = this.ctx.getImageData(this.x, this.y - 1, 60, 1);
+
+        let rgba = [];
+
+        for (let i = 0; i < pixUp.data.length; i += 4) {
+            const red = pixUp.data[i];
+            const green = pixUp.data[i + 1];
+            const blue = pixUp.data[i + 2];
+            const alpha = pixUp.data[i + 3];
+            const array = [red, green, blue, alpha];
+            rgba.push(array);
+        }
+        if (rgba[0][0] === 0 && rgba[0][1] === 0 && rgba[0][2] === 0 && rgba[0][3] === 255) {
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][3] === 255) {
+            return true
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][3] === 255) {
+
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][3] === 255) {
+
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - 1)][0] === 0 && rgba[Math.floor(rgba.length - 1)][1] === 0 && rgba[Math.floor(rgba.length - 1)][2] === 0 && rgba[Math.floor(rgba.length - 1)][3] === 255) {
+
+            return true;
+        }
+        else if (rgba[0][0] !== 255 && rgba[0][1] !== 255 && rgba[0][2] !== 255 && rgba[0][3] !== 255) {
+            if (rgba[0][0] !== 0 && rgba[0][1] !== 0 && rgba[0][2] !== 0 && rgba[0][3] !== 0) {
+                return true;
+            }
+            else {
+                return false
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+    * Logique de vérification de collision avec un mur en bas
+    * 
+    * 
+    * @returns Boolean - true => collision / false => pas de collision
+    * 
+    */
+    checkDownCollision = () => {
+        const pixDown = this.ctx.getImageData(this.x, this.y + 61, 60, 1);
+
+        let rgba = [];
+
+        for (let i = 0; i < pixDown.data.length; i += 4) {
+            const red = pixDown.data[i];
+            const green = pixDown.data[i + 1];
+            const blue = pixDown.data[i + 2];
+            const alpha = pixDown.data[i + 3];
+            const array = [red, green, blue, alpha];
+            rgba.push(array);
+        }
+        if (rgba[0][0] === 0 && rgba[0][1] === 0 && rgba[0][2] === 0 && rgba[0][3] === 255) {
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 25 / 100)][3] === 255) {
+            return true
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 50 / 100)][3] === 255) {
+
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][0] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][1] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][2] === 0 && rgba[Math.floor(rgba.length - rgba.length * 75 / 100)][3] === 255) {
+
+            return true;
+        }
+        else if (rgba[Math.floor(rgba.length - 1)][0] === 0 && rgba[Math.floor(rgba.length - 1)][1] === 0 && rgba[Math.floor(rgba.length - 1)][2] === 0 && rgba[Math.floor(rgba.length - 1)][3] === 255) {
+
+            return true;
+        }
+        else if (rgba[0][0] !== 255 && rgba[0][1] !== 255 && rgba[0][2] !== 255 && rgba[0][3] !== 255) {
+            if (rgba[0][0] !== 0 && rgba[0][1] !== 0 && rgba[0][2] !== 0 && rgba[0][3] !== 0) {
+                return true;
+            }
+            else {
+                return false
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    move = () => {
+        if (this.moveDirection === "right") {
+            if (!this.checkRightCollision() && this.checkUpCollision() && this.checkDownCollision()) {
+                this.moveRight();
+            }
+            else if ((!this.checkRightCollision() && !this.checkUpCollision() && this.checkDownCollision()) || (this.checkLeftCollision() && !this.checkUpCollision() && this.checkDownCollision())) {
+                this.moveUp();
+            }
+            else if ((!this.checkRightCollision() && this.checkUpCollision() && !this.checkDownCollision()) || (this.checkLeftCollision() && this.checkUpCollision() && !this.checkDownCollision())) {
+                this.moveDown();
+            }
+            else if (this.checkRightCollision() && !this.checkUpCollision() && !this.checkDownCollision()) {
+                const choice = Math.floor(Math.random() * 2) + 1;
+                choice === 1 ? this.moveUp() : this.moveDown();
+            }
+            else if ((this.checkRightCollision() && this.checkUpCollision() && !this.checkDownCollision()) || (this.checkRightCollision() && !this.checkUpCollision() && this.checkDownCollision())) {
+                !this.checkDownCollision() ? this.moveDown() : this.moveUp();
+            }
+            else if (!this.checkRightCollision() && !this.checkUpCollision() && !this.checkDownCollision()) {
+                const choice = Math.floor(Math.random() * 3) + 1;
+                switch (choice) {
+                    case 1: {
+                        this.moveUp();
+                        break;
+                    }
+                    case 2: {
+                        this.moveDown();
+                        break;
+                    }
+                    case 3: {
+                        this.moveRight();
+                        break;
+                    }
+                }
+            }
+            else if (this.checkRightCollision() && this.checkUpCollision() && this.checkDownCollision() && !this.checkLeftCollision()) {
+                this.moveLeft();
+            }
+        }
+        else if (this.moveDirection === "left") {
+            if (!this.checkLeftCollision() && this.checkUpCollision() && this.checkDownCollision()) {
+                this.moveLeft();
+            }
+            else if ((!this.checkLeftCollision() && !this.checkUpCollision() && this.checkDownCollision()) || (this.checkLeftCollision() && !this.checkUpCollision() && this.checkDownCollision())) {
+                this.moveUp();
+            }
+            else if ((!this.checkLeftCollision() && this.checkUpCollision() && !this.checkDownCollision()) || (this.checkLeftCollision() && this.checkUpCollision() && !this.checkDownCollision())) {
+                this.moveDown();
+            }
+            else if (this.checkLeftCollision() && !this.checkUpCollision() && !this.checkDownCollision()) {
+                const choice = Math.floor(Math.random() * 2) + 1;
+                choice === 1 ? this.moveUp() : this.moveDown();
+            }
+            else if ((this.checkLeftCollision() && this.checkUpCollision() && !this.checkDownCollision()) || (this.checkLeftCollision() && !this.checkUpCollision() && this.checkDownCollision())) {
+                !this.checkDownCollision() ? this.moveDown() : this.moveUp();
+            }
+            else if (!this.checkRightCollision() && !this.checkUpCollision() && !this.checkDownCollision()) {
+                const choice = Math.floor(Math.random() * 3) + 1;
+                switch (choice) {
+                    case 1: {
+                        this.moveUp();
+                        break;
+                    }
+                    case 2: {
+                        this.moveDown();
+                        break;
+                    }
+                    case 3: {
+                        this.moveLeft();
+                        break;
+                    }
+                }
+            }
+            else if (this.checkLeftCollision() && this.checkUpCollision() && this.checkDownCollision() && !this.checkRightCollision()) {
+                this.moveRight();
+            }
+        }
+        else if (this.moveDirection === "up") {
+            if (!this.checkUpCollision() && this.checkLeftCollision() && this.checkRightCollision()) {
+                this.moveUp();
+            }
+            else if ((!this.checkUpCollision() && !this.checkLeftCollision() && this.checkRightCollision()) || (this.checkUpCollision() && !this.checkLeftCollision() && this.checkRightCollision())) {
+                this.moveLeft();
+            }
+            else if ((!this.checkUpCollision() && this.checkLeftCollision() && !this.checkRightCollision()) || (this.checkUpCollision() && this.checkLeftCollision() && !this.checkRightCollision())) {
+                this.moveRight();
+            }
+            else if (this.checkUpCollision() && !this.checkLeftCollision() && !this.checkRightCollision()) {
+                const choice = Math.floor(Math.random() * 2) + 1;
+                choice === 1 ? this.moveLeft() : this.moveRight();
+            }
+            else if ((this.checkUpCollision() && !this.checkLeftCollision() && this.checkRightCollision()) || (this.checkUpCollision() && this.checkLeftCollision() && !this.checkRightCollision())) {
+                !this.checkLeftCollision() ? this.moveRight() : this.moveLeft();
+            }
+            else if (!this.checkUpCollision() && !this.checkLeftCollision() && !this.checkRightCollision()) {
+                const choice = Math.floor(Math.random() * 3) + 1;
+                switch (choice) {
+                    case 1: {
+                        this.moveLeft();
+                        break;
+                    }
+                    case 2: {
+                        this.moveRight();
+                        break;
+                    }
+                    case 3: {
+                        this.moveUp();
+                        break;
+                    }
+                }
+            }
+            else if (this.checkUpCollision() && this.checkLeftCollision() && this.checkRightCollision() && !this.checkDownCollision()) {
+                this.moveDown();
+            }
+        }
+        else if (this.moveDirection === "down") {
+            if (!this.checkDownCollision() && this.checkLeftCollision() && this.checkRightCollision()) {
+                this.moveDown();
+            }
+            else if ((!this.checkDownCollision() && !this.checkLeftCollision() && this.checkRightCollision()) || (this.checkDownCollision() && !this.checkLeftCollision() && this.checkRightCollision())) {
+                this.moveLeft()
+            }
+            else if ((!this.checkDownCollision() && this.checkLeftCollision() && !this.checkRightCollision()) || (this.checkDownCollision() && this.checkLeftCollision() && !this.checkRightCollision())) {
+                this.moveRight();
+            }
+            else if (this.checkDownCollision() && !this.checkLeftCollision() && !this.checkRightCollision()) {
+                const choice = Math.floor(Math.random() * 2) + 1;
+                choice === 1 ? this.moveLeft() : this.moveRight();
+            }
+            else if ((this.checkDownCollision() && this.checkLeftCollision() && !this.checkRightCollision()) || (this.checkUpCollision() && !this.checkLeftCollision() && this.checkRightCollision())) {
+                !this.checkLeftCollision() ? this.moveRight() : this.moveLeft();
+            }
+            else if (!this.checkUpCollision() && !this.checkLeftCollision() && !this.checkRightCollision()) {
+                const choice = Math.floor(Math.random() * 3) + 1;
+                switch (choice) {
+                    case 1: {
+                        this.moveLeft();
+                        break;
+                    }
+                    case 2: {
+                        this.moveRight();
+                        break;
+                    }
+                    case 3: {
+                        this.moveDown();
+                        break;
+                    }
+                }
+            }
+            else if (this.checkDownCollision() && this.checkLeftCollision() && this.checkRightCollision() && !this.checkUpCollision()) {
+                this.moveUp();
+            }
+        }
     }
 
     moveRight = () => {
-
         this.moveDirection = "right";
         this.ctx.clearRect(this.x, this.y, this.width, this.height);
         this.x += this.speed;
@@ -106,7 +411,6 @@ class Hostile {
     }
 
     moveLeft = () => {
-
         this.moveDirection = "left";
         this.ctx.clearRect(this.x, this.y, this.width, this.height);
         this.x -= this.speed;
@@ -115,7 +419,6 @@ class Hostile {
     }
 
     moveUp = () => {
-
         this.moveDirection = "up";
         this.ctx.clearRect(this.x, this.y, this.width, this.height);
         this.y -= this.speed;
@@ -124,7 +427,6 @@ class Hostile {
     }
 
     moveDown = () => {
-
         this.moveDirection = "down";
         this.ctx.clearRect(this.x, this.y, this.width, this.height);
         this.y += this.speed;
